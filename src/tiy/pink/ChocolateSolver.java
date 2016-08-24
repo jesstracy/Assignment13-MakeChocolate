fe11125d6a2233eb940e4637947226c9296cb0c9
Jessica Tracy <j.tracy916@gmail.com>
2016-08-24 13:28:33 -0400

    Made makeChocolate2() method that will return the smalls AND bigs

--- src/tiy/pink/ChocolateSolver.java
+++ src/tiy/pink/ChocolateSolver.java
@@ -5,6 +5,7 @@ package tiy.pink;
  */
 public class ChocolateSolver {
 
+    //Returns number of smalls
     public int makeChocolate(int small, int big, int goal) {
         if(small < 0 || big < 0 || goal < 0) {
             return -1;
@@ -26,5 +27,36 @@ public class ChocolateSolver {
                 return -1;
             }
         }
+    }
+
+    //Returns number of smalls AND number of bigs
+    public int[] makeChocolate2(int small, int big, int goal) {
+        int[] returnArraySmallBig = new int[2];
+        if(small < 0 || big < 0 || goal < 0) {
+            returnArraySmallBig[0] = -1;
+            returnArraySmallBig[1] = -1;
+        }
+        int greatestMultipleOfFiveInGoal = goal - (goal % 5);
+        int numBigsNeededToMakeGreatestMultipleOfFiveInGoal = greatestMultipleOfFiveInGoal / 5;
+        if (big <= numBigsNeededToMakeGreatestMultipleOfFiveInGoal) {
+            int smallsNeeded = goal - (5 * big);
+            if (small >= smallsNeeded) {
+                returnArraySmallBig[0] = smallsNeeded;
+                returnArraySmallBig[1] = big;
+            } else {
+                returnArraySmallBig[0] = -1;
+                returnArraySmallBig[1] = -1;
+            }
+        } else {
+            int smallsNeeded = goal - (5 * numBigsNeededToMakeGreatestMultipleOfFiveInGoal);
+            if (small >= smallsNeeded) {
+                returnArraySmallBig[0] = smallsNeeded;
+                returnArraySmallBig[1] = numBigsNeededToMakeGreatestMultipleOfFiveInGoal;
+            } else {
+                returnArraySmallBig[0] = -1;
+                returnArraySmallBig[1] = -1;
+            }
+        }
+        return returnArraySmallBig;
     }
 }
--- test/tiy/pink/ChocolateSolverTest.java
+++ test/tiy/pink/ChocolateSolverTest.java
@@ -23,6 +23,14 @@ public class ChocolateSolverTest {
 
     }
 
+//******************************************************************************
+//Tests for makeChocolate method
+    @Test
+    public void testMisc() throws Exception {
+        int numSmalls = solver.makeChocolate(12, 5, 19);
+        assertEquals(4, numSmalls);
+    }
+
     @Test
     public void testEnoughSmallsNoBigs() throws Exception {
         int numSmalls = solver.makeChocolate(12, 0, 7);
@@ -130,12 +138,25 @@ public class ChocolateSolverTest {
     public void lotsOfSmallsCantBeDoneWithOnlyBigs() throws Exception {
         int numSmalls = solver.makeChocolate(75, 2, 70);
         assertEquals(60, numSmalls);
+
     }
 
     @Test
     public void lotsOfSmallsNoBigs() throws Exception {
         int numSmalls = solver.makeChocolate(75, 0, 70);
         assertEquals(70, numSmalls);
+    }
+
+    @Test
+    public void exactNumberWorks() throws Exception {
+        int numSmalls = solver.makeChocolate(5, 4, 25);
+        assertEquals(5, numSmalls);
+    }
+
+    @Test
+    public void notEnoughBigsAlmostEnoughSmall() throws Exception {
+        int numSmalls = solver.makeChocolate(6, 7, 42);
+        assertEquals(-1, numSmalls);
     }
 
     @Test
@@ -155,4 +176,158 @@ public class ChocolateSolverTest {
         int numSmalls = solver.makeChocolate(4, 1, 7);
         assertEquals(2, numSmalls);
     }
+
+//******************************************************************************
+//Tests for makeChocolate2 method
+//    @Test
+//    public void testMisc() throws Exception {
+//        int numSmalls = solver.makeChocolate2(12, 5, 19);
+//        assertEquals(4, numSmalls);
+//    }
+//
+//    @Test
+//    public void testEnoughSmallsNoBigs() throws Exception {
+//        int numSmalls = solver.makeChocolate2(12, 0, 7);
+//        assertEquals(7, numSmalls);
+//    }
+//
+//    @Test
+//    public void testEnoughBigsNoSmalls() throws Exception {
+//        int numSmalls = solver.makeChocolate2(0, 2, 10);
+//        assertEquals(0, numSmalls);
+//    }
+//
+//    @Test
+//    public void testNoBigsNoSmallsEquals0() throws Exception {
+//        int numSmalls = solver.makeChocolate2(0, 0, 0);
+//        assertEquals(0, numSmalls);
+//    }
+//
+//    @Test
+//    public void testNoBigsNoSmallsEquals1() throws Exception {
+//        int numSmalls = solver.makeChocolate2(0, 0, 1);
+//        assertEquals(-1, numSmalls);
+//    }
+//
+//    @Test
+//    public void enoughBigsEnoughSmalls() throws Exception {
+//        int numSmalls = solver.makeChocolate2(9, 3, 19);
+//        assertEquals(4, numSmalls);
+//    }
+//
+//    @Test
+//    public void negativeBigs() throws Exception {
+//        int numSmalls = solver.makeChocolate2(2, -5, 25);
+//        assertEquals(-1, numSmalls);
+//    }
+//
+//
+//    @Test
+//    public void negativeBigsWithLargeNumberSmalls2() throws Exception {
+//        int numSmalls = solver.makeChocolate(50, -5, 25);
+//        assertEquals(-1, numSmalls);
+//    }
+//
+//    @Test
+//    public void negativeSmalls() throws Exception {
+//        int numSmalls = solver.makeChocolate2(-3, 1, 8);
+//        assertEquals(-1, numSmalls);
+//    }
+//
+//    @Test
+//    public void negativeSmallsWithLargeNumberBigs() throws Exception {
+//        int numSmalls = solver.makeChocolate2(-3, 51, 8);
+//        assertEquals(-1, numSmalls);
+//    }
+//
+//    @Test
+//    public void moreBigsThanNeededPossible() throws Exception {
+//        int numSmalls = solver.makeChocolate2(3, 4, 12);
+//        assertEquals(2, numSmalls);
+//    }
+//
+//    @Test
+//    public void moreBigsThanNeededNotPossible() throws Exception {
+//        int numSmalls = solver.makeChocolate2(2, 6, 18);
+//        assertEquals(-1, numSmalls);
+//    }
+//
+//    @Test
+//    public void enoughSmallsNotEnoughBigsButNotZero() throws Exception {
+//        int numSmalls = solver.makeChocolate2(30, 1, 11);
+//        assertEquals(6, numSmalls);
+//    }
+//
+//    @Test
+//    public void sameAmountOfSmallsAsBigs() throws Exception {
+//        int numSmalls = solver.makeChocolate2(15, 3, 15);
+//        assertEquals(0, numSmalls);
+//    }
+//
+//    @Test
+//    public void moreSmallsAndBigsThanGoal() throws Exception {
+//        int numSmalls = solver.makeChocolate2(7, 2, 1);
+//        assertEquals(1, numSmalls);
+//    }
+//
+//    @Test
+//    public void goalZeroEnoughBigsEnoughSmalls() throws Exception {
+//        int numSmalls = solver.makeChocolate2(7, 2, 0);
+//        assertEquals(0, numSmalls);
+//    }
+//
+//    @Test
+//    public void notEnoughSmallsNotEnoughBigs() throws Exception {
+//        int numSmalls = solver.makeChocolate2(17, 3, 100);
+//        assertEquals(-1, numSmalls);
+//    }
+//
+//    @Test
+//    public void lotsOfSmallsButCanBeDoneWithBigs() throws Exception {
+//        int numSmalls = solver.makeChocolate2(100, 20, 100);
+//        assertEquals(0, numSmalls);
+//    }
+//
+//    @Test
+//    public void lotsOfSmallsCantBeDoneWithOnlyBigs() throws Exception {
+//        int numSmalls = solver.makeChocolate2(75, 2, 70);
+//        assertEquals(60, numSmalls);
+//
+//    }
+//
+//    @Test
+//    public void lotsOfSmallsNoBigs() throws Exception {
+//        int numSmalls = solver.makeChocolate2(75, 0, 70);
+//        assertEquals(70, numSmalls);
+//    }
+//
+//    @Test
+//    public void exactNumberWorks() throws Exception {
+//        int numSmalls = solver.makeChocolate2(5, 4, 25);
+//        assertEquals(5, numSmalls);
+//    }
+//
+//    @Test
+//    public void notEnoughBigsAlmostEnoughSmall() throws Exception {
+//        int numSmalls = solver.makeChocolate2(6, 7, 42);
+//        assertEquals(-1, numSmalls);
+//    }
+//
+//    @Test
+//    public void exampleTestFromSite1() throws Exception {
+//        int numSmalls = solver.makeChocolate2(4, 1, 9);
+//        assertEquals(4, numSmalls);
+//    }
+//
+//    @Test
+//    public void exampleTestFromSite2() throws Exception {
+//        int numSmalls = solver.makeChocolate2(4, 1, 10);
+//        assertEquals(-1, numSmalls);
+//    }
+//
+//    @Test
+//    public void exampleTestFromSite3() throws Exception {
+//        int numSmalls = solver.makeChocolate2(4, 1, 7);
+//        assertEquals(2, numSmalls);
+//    }
 }
